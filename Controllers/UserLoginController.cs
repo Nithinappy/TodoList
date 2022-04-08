cccecb19f4ccaa6ad6cb34020790debee1b0bd24
Nithin <69035664+Nithinappy@users.noreply.github.com>
2022-04-08 11:29:53 +0530

    Final Commit

--- Controllers/UserLoginController.cs
+++ Controllers/UserLoginController.cs
@@ -53,7 +53,7 @@ public class UserLoginController : ControllerBase
         var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
             _configuration["Jwt:Audience"],
             claims,
-            expires: DateTime.Now.AddMinutes(15),
+            expires: DateTime.Now.AddMinutes(30),
             signingCredentials: credentials);
 
         return new JwtSecurityTokenHandler().WriteToken(token);
